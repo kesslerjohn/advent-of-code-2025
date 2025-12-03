3d3f4ce9453d3390f7e0c76c54bf831ee7c3087d
kesslerjohn <kesslerjohn@outlook.com>
2025-12-03 17:06:58 -0600

    Refactor Dial.js for simplicity

--- js/day1/app.js
+++ js/day1/app.js
@@ -4,7 +4,7 @@ import { readFile } from 'fs';
 import { Dial } from './dial.js';
 
 (() => {
-    const dial = new Dial(99, 50);
+    const dial = new Dial(100, 50);
 
     readFile('../../inputs/day1.txt', 'utf8', (err, data) => {
         if (err) {
--- js/day1/dial.js
+++ js/day1/dial.js
@@ -5,27 +5,50 @@ export class Dial {
 	#count;
 
 	constructor(size, start) {
-		this.#size = size+1;
+		this.#size = size;
 		this.#pos = start;
 		this.#count = 0;
 	}
 
     #resolveDirection(line) {
-        return line
+        return +(line
                 .replace(/^L/, "-")
-                .replace(/^R/, "");
+                .replace(/^R/, ""));
     }
 
     #mod(dividend, divisor) {
         return ((dividend % divisor) + divisor) % divisor;
     }
 
+    #countFullTurns(dividend, divisor) {
+        return Math.floor(Math.abs(dividend)/divisor);
+    }
+
 	turn(line) {
         let direction = this.#resolveDirection(line);
-		this.#pos = this.#mod(this.#pos + +(direction), this.#size);
-		if (this.#pos == 0){
-			this.#count += 1;
-		}
+        let absolutePos = this.#pos + direction;
+
+        // always check how many full rotations we make
+        this.#count += this.#countFullTurns(direction, this.#size);
+
+        // only check partial turns if we are not starting at 0
+        if (this.#pos !== 0) {
+
+            if (absolutePos < 0 || absolutePos >= this.#size+1){
+                this.#count += 1;
+            }
+
+        }
+
+        // finally, if you land on 0, count that. 
+        this.#pos = this.#mod(absolutePos, this.#size);
+
+        if (this.#pos === 0) {
+            this.#count += 1;
+        }
+
+        console.log(monitor);
+
 	}
 
 	get count() {
