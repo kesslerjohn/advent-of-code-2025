c7a9814586d126be13fc14afd309e858582ade3a
kesslerjohn <kesslerjohn@outlook.com>
2025-12-05 15:27:05 -0600

    add logic to validate one id

--- js/day2/validator.js
+++ js/day2/validator.js
@@ -3,7 +3,22 @@ export class IDValidator {
 
     }
 
-    validateID(val) {
-        
+    #stringDivisorInd(val, part){
+        if (val.split(part).join('') == '') {
+            return true;
+        }
+        return false;
+    }
+
+    #validateID(val) {
+        let part;
+        let sum = 0;
+        for (let i = 1; i <= Math.floor(val.length/2); i++) {
+            part = val.substr(0, i);
+            if (this.#stringDivisorInd(val, part)) {
+                sum += +(val);
+                break;
+            }
+        }
     }
 }
