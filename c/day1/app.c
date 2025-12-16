3a4aac4faa0c4fe7ecd2af0a4b8e6d2b65e02475
kesslerjohn <kesslerjohn@outlook.com>
2025-12-15 19:32:47 -0600

    introduce more examples of usage for dial

--- c/day1/app.c
+++ c/day1/app.c
@@ -10,5 +10,24 @@ int main() {
 
     printf("The dial is pointed to: %s\n", pos);
 
+    char turn[4] = "L10";
+
+    int mult = resolveDirection(turn);
+
+    // when cast to string, the result of mult will be either
+    // length 3 ("-1\0") or length 2 ("1\0")
+    int size;
+    if (mult < 0) {
+        size = 3;
+    } else {
+        size = 2;
+    }
+
+    char dir[size];
+
+    sprintf(dir, "%d", mult);
+
+    printf("Turning direction is: %s\n", dir);
+
     return 0;
 }
--- c/day1/dial.h
+++ c/day1/dial.h
@@ -1,6 +1,8 @@
 #ifndef _DIAL_H
 #define _DIAL_H
 
+#include <string.h>
+
 struct dial {
     int size;
     int pos;
@@ -25,6 +27,22 @@ int countFullTurns(int dividend, int divisor) {
 
 int checkPartialClicks(int dividend, int divisor) {
     return dividend%divisor;
+}
+
+int resolveDirection(char *line) {
+    // determine sign by line[0];
+    // the length of line is not fixed
+    int mult;
+    switch (line[0]){
+        case 'L':
+            mult = -1;
+            break;
+        default:
+            mult = 1;
+            break;
+    }
+
+    return mult;
 }
 
 #endif
